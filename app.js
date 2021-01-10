5204d4b6a58a2df0808317f0b96a08288c077b38
jadekang <jkang.se@gmail.com>
2021-01-09 16:10:51 -0800

    Created MongoDB and connected my api to the DB

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+
+// Middlewares
 
 // Create ROUTES
 app.get('/', (req, res, next) => {
   res.send('We are at home');
 });
+
+// Connect to my DB
+mongoose.connect(
+  process.env.DB_CONNECTIONS,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log('DB connected')
+);
 
 // Add a method to listen to my server
 app.listen(3000);
