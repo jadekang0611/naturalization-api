5415942d7d06ae7cfd3401b36a50566343e608f8
jadekang <jkang.se@gmail.com>
2021-01-10 22:04:30 -0800

    Implemented the POST a new card route testing

--- test/appTest.js
+++ test/appTest.js
@@ -18,7 +18,7 @@ describe('Cards', () => {
   });
 });
 
-/* -------Test the /GET routes------- */
+/* -------Test the /GET route------- */
 describe('/GET card', () => {
   it('it should GET all cards', (done) => {
     chai
@@ -27,8 +27,58 @@ describe('/GET card', () => {
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
-        res.body.length.should.be.eql(3);
         done();
       });
   });
 });
+
+/* -------Test the /GET by id route------- */
+// describe('/GET/:cardId', () => {
+//   it('it should GET a card by the given id', (done) => {
+//     let card = new Card({
+//       question: 'testing question',
+//       answer: 'testing answer',
+//       category: 'testing category',
+//     });
+//     card.save((err, card) => {
+//       chai
+//         .request(app)
+//         .get('/cards/' + card.id)
+//         .send(card)
+//         .end((err, res) => {
+//           res.should.have.status(200);
+//           res.body.should.have.a('object');
+//           res.body.should.have.property('question');
+//           res.body.should.have.property('answer');
+//           res.body.should.have.property('category');
+//           res.body.should.have.property('_id').equal(card.id);
+//           done();
+//         });
+//     });
+//   });
+// });
+
+/* -------Test the /POST route------- */
+// describe('POST card', () => {
+//   it('it should only POST if card has all fields', (done) => {
+//     let card = {
+//       question: 'testing question',
+//       answer: 'testing answer',
+//       category: 'testing category',
+//     };
+//     chai
+//       .request(app)
+//       .post('/cards')
+//       .send(card)
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         res.body.should.be.a('object');
+//         res.body.should.have.property('question');
+//         res.body.should.have.property('answer');
+//         res.body.should.have.property('category');
+//         done();
+//       });
+//   });
+// });
+
+
